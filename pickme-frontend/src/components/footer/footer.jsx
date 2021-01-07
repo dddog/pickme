cc679f2c5386c397f0eb3b8533e3414f97f2219a
dddog <azskw1101@gmail.com>
2021-01-07 16:39:04 +0900

    footer 문의 링크 작업

--- pickme-frontend/src/components/footer/footer.jsx
+++ pickme-frontend/src/components/footer/footer.jsx
@@ -3,7 +3,12 @@ import styles from "./footer.module.css";
 
 const Footer = memo(() => (
   <footer className={styles.footer}>
-    <p className={styles.title}>문의하기</p>
+    <p className={styles.title}>
+      <span className={styles.text}>문의하기</span>
+      <a className={styles.contact} href="mailto:dddog0000@gmail.com">
+        <span>dddog0000@gmail.com</span>
+      </a>
+    </p>
   </footer>
 ));
 
