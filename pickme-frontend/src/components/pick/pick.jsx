590762ddc5d735ac8a031193dfb9f267150e43f2
dddog <azskw1101@gmail.com>
2021-01-11 12:29:45 +0900

    pick 링크 걸기

--- pickme-frontend/src/components/pick/pick.jsx
+++ pickme-frontend/src/components/pick/pick.jsx
@@ -3,15 +3,17 @@ import styles from "./pick.module.css";
 
 const Pick = ({ img }) => (
   <div className={styles.pick}>
-    <img
-      className={styles.img}
-      key={img.key}
-      src={img.fileUrl}
-      alt={img.title}
-    ></img>
-    <div className={styles.container}>
-      <h4>{img.title}</h4>
-    </div>
+    <a className={styles.link} href="#">
+      <img
+        className={styles.img}
+        key={img.key}
+        src={img.fileUrl}
+        alt={img.title}
+      ></img>
+      <div className={styles.container}>
+        <h4>{img.title}</h4>
+      </div>
+    </a>
   </div>
 );
 
